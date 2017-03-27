f7e2e2abb78685802beeb36978d9313031757038
Elton Goncalves <elton@XS2-MacBook-Pro.local>
2017-03-28 00:34:11 +0200

    Added service worker that caches pages for offline usage and a manifest that links to icons.

---
+++ public/sw.js
@@ -0,0 +1,57 @@
+// Simple example
+var CACHE_NAME_CORE = 'funda-v1-core';
+var CACHE_NAME_PAGES = 'funda-v1-pages';
+var urlsToCache = [
+    '/css/style.css',
+    '/img/*.svg',
+    '/img/*.png'
+];
+
+self.addEventListener('install', event => event.waitUntil(
+    caches.open(CACHE_NAME)
+        .then(cache => {console.log('Opened cache'); return cache.addAll(urlsToCache)})
+        .then(self.skipWaiting())
+        .catch(function(err) {console.error(err);})
+
+));
+
+
+self.addEventListener('fetch', function(event) {
+    const request = event.request;
+    console.log(request);
+    if (request.mode === 'navigate') {
+        event.respondWith(
+            fetch(request)
+                .then(response => cachePage(request, response))
+                .catch(err => getCachedPage(request))
+                .catch(err => fetchCoreFile('/offline/'))
+        );
+    } else {
+        event.respondWith(
+            fetch(request)
+                .catch(err => fetchCoreFile(request.url))
+                .catch(err => fetchCoreFile('/offline/'))
+        );
+    }
+});
+
+
+
+function fetchCoreFile(url) {
+    return caches.open(CACHE_NAME_CORE)
+        .then(cache => cache.match(url))
+        .then(response => response ? response : Promise.reject());
+}
+
+function getCachedPage(request) {
+    return caches.open(CACHE_NAME_PAGES)
+        .then(cache => cache.match(request))
+        .then(response => response ? response : Promise.reject());
+}
+
+function cachePage(request, response) {
+    const clonedResponse = response.clone();
+    caches.open(CACHE_NAME_PAGES)
+        .then(cache => cache.put(request, clonedResponse));
+    return response;
+}
